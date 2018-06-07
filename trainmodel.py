5be8a6526fc14b219f9d0579342dfddd06c5c54b
kevinxie96 <kevinxie96@g.ucla.edu>
2018-06-07 15:35:39 -0700

    Update trainmodel.py

--- trainmodel.py
+++ trainmodel.py
@@ -1,14 +1,19 @@
 def train_model():
     stop_increment = 3
     min_increment = 0
+    test_batch_amount = 100 or a high enough amount to check 
+                        that we have reached a local min
     for each epoch:
         for each batch:
-            calculate the batch loss by running the batch
+            calculate the batch loss/error by running the batch
             through the model we built with build_model()
-            if we have reached a new minimum:
-                min_increment += 1
-            if min_increment == stop_increment:
-                break
+            if we have reached a 
+                if we have reached a new minimum:
+                    min_increment = 0
+                else if there is no improvement:
+                    min_increment = 1
+                    if min_increment == stop_increment:
+                        break
         if min_increment == stop_increment:
             save the model in disk as a checkpoint
             return
